ec9cd3d76aef098fa907d551903e0671bf55278b
KumJungMin <rmawjdals@naver.com>
2022-04-07 18:32:27 +0900

    개방 폐쇄 원칙에 대한 간단한 예시

--- comment-list/src/commentComposerFactory.js
+++ comment-list/src/commentComposerFactory.js
@@ -1,5 +1,24 @@
 import { v4 as uuidv4 } from "uuid";
+import commentRefiner from "../src/content-refiners/compactWhitespaces";
 
+// 1. 초기 코드
+// commentRefiner라는 함수에 의존적인 상태
+function commentComposerFactory1() {
+  return ({ author, content }) => {
+    return {
+      id: uuidv4(),
+      author,
+      content: commentRefiner(content),
+      createdTime: new Date(),
+    };
+  };
+}
+
+//  2. commentRefiner를 인자로 넘기는 경우
+// commentRefiner라는 구체적인 함수에 의존하지 않는다.
+// 심지어 기획이 변경되어, commentRefiner를 다른 기능으로 바꿀 때,
+// commentComposerFactory를 변경하지 않아도 된다!(환경 적응력이 높은 코드)
+// 이런 형태의 코드는 단위 테스트가 용이하다
 function commentComposerFactory({ commentRefiner }) {
   return ({ author, content }) => {
     return {
--- comment-list/src/index.js
+++ comment-list/src/index.js
@@ -7,6 +7,9 @@ import trimWhitespaces from "./content-refiners/trimWhitespaces";
 import maskBannedWords from "./content-refiners/maskBannedWords";
 import commentComposerFactory from "./commentComposerFactory";
 
+// 포맷팅하는 함수를 인자로 넘기는 것만으로도
+// commentComposerFactory 내부 로직은 건드리지 않을 수 있다.
+// 이 함수를 이용하는 index.js는 코드의 변경이 불가피하지만, 그 함수 자체(raw)는 변화에 영향을 받지 않는다!
 const commentRefiner = compositeContentRefinerFactory([
   compactWhitespaces,
   trimWhitespaces,
